1394035fbc6efcc2238685eac33b9f950cd08f19
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2023-02-14 12:26:42 -0500

    made single components

---
+++ reactportfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import react from 'react'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello get caught up</h1>
+    </div>
+  );
+}
+
+export default App;
