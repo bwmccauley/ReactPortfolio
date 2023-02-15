6ebbd48af9d01cb755fed7cfb41b1d763ea3f600
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2023-02-15 16:18:32 -0500

    added routes and app files

---
+++ reactportfolio/src/app/routes.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, Routes} from "react-router-dom";
+import withRouter from "../hooks/withRouter"
+import { Home } from "../routes/home";
+import { Portfolio } from "../routes/portfolio";
+import { ContactUs } from "../routes/contact";
+import { About } from "../routes/about";
+import { Socialicons } from "../components/socialicons";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const AnimatedRoutes = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition
+      key={location.key}
+      timeout={{
+        enter: 400,
+        exit: 400,
+      }}
+      classNames="route"
+      unmountOnExit
+    >
+      <Routes location={location}>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+function AppRoutes() {
+  return (
+    <div className="s_c">
+      <AnimatedRoutes />
+      <Socialicons />
+    </div>
+  );
+}
+
+export default AppRoutes
